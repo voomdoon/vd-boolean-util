0b8e86c428883461e03a110d8ee8a3a457cff9b9
voomdoon <nilreb24-github20141105@yahoo.de>
2025-06-02 17:45:57 +0200

    feat: support format 1/0
    references: #1

--- src/main/java/de/voomdoon/util/bool/BooleanMatrixFormatter.java
+++ src/main/java/de/voomdoon/util/bool/BooleanMatrixFormatter.java
@@ -10,6 +10,33 @@ package de.voomdoon.util.bool;
 public class BooleanMatrixFormatter {
 
 	/**
+	 * DOCME add JavaDoc for BooleanMatrixFormatter
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	public enum Format {
+
+		/**
+		 * @since 0.1.0
+		 */
+		ONE_ZERO,
+
+		/**
+		 * @since 0.1.0
+		 */
+		TRUE_FALSE,
+
+		;
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	private Format format = Format.TRUE_FALSE;
+
+	/**
 	 * DOCME add JavaDoc for method format
 	 * 
 	 * @param matrix
@@ -29,16 +56,41 @@ public class BooleanMatrixFormatter {
 			}
 
 			for (int iColumn = 0; iColumn < matrix[iRow].length; iColumn++) {
-				if (iColumn > 0) {
+				if (format == Format.TRUE_FALSE && iColumn > 0) {
 					sb.append(",");
 				}
 
-				sb.append(matrix[iRow][iColumn]);
+				sb.append(format(matrix[iRow][iColumn]));
 
 			}
 		}
 
 		return sb.toString();
 		// TODO implement format
+	}
+
+	/**
+	 * DOCME add JavaDoc for method setFormat
+	 * 
+	 * @param format
+	 * @since 0.1.0
+	 */
+	public void withFormat(Format format) {
+		this.format = format;
+	}
+
+	/**
+	 * DOCME add JavaDoc for method format
+	 * 
+	 * @param b
+	 * @return
+	 * @since 0.1.0
+	 */
+	private String format(boolean b) {
+		return switch (format) {
+			case TRUE_FALSE -> Boolean.toString(b);
+			case ONE_ZERO -> b ? "1" : "0";
+			default -> throw new UnsupportedOperationException("Format '" + format + "' not supported!");
+		};
 	}
 }
--- src/test/java/de/voomdoon/util/bool/BooleanMatrixFormatterTest.java
+++ src/test/java/de/voomdoon/util/bool/BooleanMatrixFormatterTest.java
@@ -2,8 +2,11 @@ package de.voomdoon.util.bool;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import de.voomdoon.util.bool.BooleanMatrixFormatter.Format;
 
 /**
  * DOCME add JavaDoc for
@@ -17,7 +20,7 @@ class BooleanMatrixFormatterTest {
 	/**
 	 * @author André Schulz
 	 *
-	 * @since DOCME add inception version number
+	 * @since 0.1.0
 	 */
 	@Nested
 	class BasicsTest extends TestBase {
@@ -43,13 +46,68 @@ class BooleanMatrixFormatterTest {
 	 * @since 0.1.0
 	 */
 	@Nested
+	class Format_1_0_Test extends TestBase {
+
+		/**
+		 * @since 0.1.0
+		 */
+		@BeforeEach
+		void beforeEach_configureFormatter() {
+			formatter.withFormat(Format.ONE_ZERO);
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_columnSeparator_none() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { true, true } });
+
+			assertThat(actual).isEqualTo("11");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_singleValue_false() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { false } });
+
+			assertThat(actual).isEqualTo("0");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_singleValue_true() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { true } });
+
+			assertThat(actual).isEqualTo("1");
+		}
+	}
+
+	/**
+	 * DOCME add JavaDoc for BooleanMatrixFormatterTest
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	@Nested
 	class Format_trueFalse_Test extends TestBase {
 
 		/**
 		 * @since 0.1.0
 		 */
 		@Test
-		void test_columnSeparator() throws Exception {
+		void test_columnSeparator_default() throws Exception {
 			logTestStart();
 
 			String actual = formatter.format(new boolean[][] { { true, true } });
@@ -97,7 +155,7 @@ class BooleanMatrixFormatterTest {
 	/**
 	 * @author André Schulz
 	 *
-	 * @since DOCME add inception version number
+	 * @since 0.1.0
 	 */
 	private abstract class TestBase extends de.voomdoon.testing.tests.TestBase {
 
