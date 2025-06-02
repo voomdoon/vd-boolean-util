20888470764096e17bdda3c200ea5c3db547b6c5
voomdoon <nilreb24-github20141105@yahoo.de>
2025-06-02 21:35:01 +0200

    feat: support format with half line blocks
    references: #1

--- src/main/java/de/voomdoon/util/bool/BooleanMatrixFormatter.java
+++ src/main/java/de/voomdoon/util/bool/BooleanMatrixFormatter.java
@@ -1,5 +1,8 @@
 package de.voomdoon.util.bool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * DOCME add JavaDoc for
  *
@@ -26,6 +29,11 @@ public class BooleanMatrixFormatter {
 		/**
 		 * @since 0.1.0
 		 */
+		HALF_LINE_BLOCKS,
+
+		/**
+		 * @since 0.1.0
+		 */
 		ONE_ZERO,
 
 		/**
@@ -35,6 +43,139 @@ public class BooleanMatrixFormatter {
 
 		;
 	}
+
+	/**
+	 * DOCME add JavaDoc for BooleanMatrixFormatter
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	private static class HalfLineBlocksFormatter {
+
+		/**
+		 * DOCME add JavaDoc for method addFirstPassElement
+		 * 
+		 * @param row
+		 * @param outputRow
+		 * @param iColumn
+		 * @since 0.1.0
+		 */
+		private void addFirstPassElement(boolean[] row, List<String> outputRow, int iColumn) {
+			if (row[iColumn]) {
+				outputRow.add(TOP);
+			} else {
+				outputRow.add(EMPTY);
+			}
+		}
+
+		/**
+		 * @param sb
+		 * @param outputRow
+		 * @since 0.1.0
+		 */
+		private void appenNewLine(StringBuilder sb, List<String> outputRow) {
+			if (sb.length() > 0) {
+				sb.append("\n");
+			}
+
+			outputRow.forEach(sb::append);
+		}
+
+		/**
+		 * DOCME add JavaDoc for method apply
+		 * 
+		 * @param row
+		 * @param outputRow
+		 * @param pass
+		 * @since 0.1.0
+		 */
+		private void apply(boolean[] row, List<String> outputRow, int pass) {
+			for (int iColumn = 0; iColumn < row.length; iColumn++) {
+				if (pass == 1) {
+					addFirstPassElement(row, outputRow, iColumn);
+				} else {
+					updateSecondPassElement(row, outputRow, iColumn);
+				}
+			}
+		}
+
+		/**
+		 * DOCME add JavaDoc for method formatHalfLineBlocks
+		 * 
+		 * @param matrix
+		 * @return
+		 * @since 0.1.0
+		 */
+		private String format(boolean[][] matrix) {
+			StringBuilder sb = new StringBuilder();
+
+			List<String> outputRow = new ArrayList<>();
+			int pass = 0;
+
+			for (int iRow = 0; iRow < matrix.length; iRow++) {
+				pass++;
+
+				apply(matrix[iRow], outputRow, pass);
+
+				if (pass == 2) {
+					appenNewLine(sb, outputRow);
+					outputRow.clear();
+					pass = 0;
+				}
+			}
+
+			if (!outputRow.isEmpty()) {
+				appenNewLine(sb, outputRow);
+			}
+
+			return sb.toString();
+		}
+
+		/**
+		 * 
+		 * DOCME add JavaDoc for method updateSecondPassElement
+		 * 
+		 * @param row
+		 * @param outputRow
+		 * @param iColumn
+		 * @since 0.1.0
+		 */
+		private void updateSecondPassElement(boolean[] row, List<String> outputRow, int iColumn) {
+			if (row[iColumn]) {
+				if (outputRow.get(iColumn).equals(TOP)) {
+					outputRow.set(iColumn, BOTH);
+				} else {
+					outputRow.set(iColumn, BOTTOM);
+				}
+			}
+		}
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	private static final String BOTH = "█";
+
+	/**
+	 * @since 0.1.0
+	 */
+	private static final String BOTTOM = "▄";
+
+	/**
+	 * @since 0.1.0
+	 */
+	private static final String EMPTY = " ";
+
+	/**
+	 * @since 0.1.0
+	 */
+	private static final HalfLineBlocksFormatter HALF_LINE_BLOCKS_FORMATTER = new HalfLineBlocksFormatter();
+
+	/**
+	 * @since 0.1.0
+	 */
+	private static final String TOP = "▀";
 
 	/**
 	 * @since 0.1.0
@@ -51,6 +192,8 @@ public class BooleanMatrixFormatter {
 	public String format(boolean[][] matrix) {
 		if (matrix.length == 0) {
 			return "";
+		} else if (format == Format.HALF_LINE_BLOCKS) {
+			return HALF_LINE_BLOCKS_FORMATTER.format(matrix);
 		}
 
 		StringBuilder sb = new StringBuilder();
@@ -99,4 +242,5 @@ public class BooleanMatrixFormatter {
 			default -> throw new UnsupportedOperationException("Format '" + format + "' not supported!");
 		};
 	}
+
 }
--- src/test/java/de/voomdoon/util/bool/BooleanMatrixFormatterTest.java
+++ src/test/java/de/voomdoon/util/bool/BooleanMatrixFormatterTest.java
@@ -168,6 +168,124 @@ class BooleanMatrixFormatterTest {
 	 * @since 0.1.0
 	 */
 	@Nested
+	class Format_halfLineBlocks_Test extends TestBase {
+
+		/**
+		 * @since 0.1.0
+		 */
+		@BeforeEach
+		void beforeEach_configureFormatter() {
+			formatter.withFormat(Format.HALF_LINE_BLOCKS);
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_2Rows_falseFalse() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { false }, { false } });
+
+			assertThat(actual).isEqualTo(" ");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_2Rows_falseTrue() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { false }, { true } });
+
+			assertThat(actual).isEqualTo("▄");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_2Rows_trueFalse() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { true }, { false } });
+
+			assertThat(actual).isEqualTo("▀");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_2Rows_trueTrue() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { true }, { true } });
+
+			assertThat(actual).isEqualTo("█");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_3Rows() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { true }, { true }, { true } });
+
+			assertThat(actual).isEqualTo("█\n▀");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_singleValue_false() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { false } });
+
+			assertThat(actual).isEqualTo(" ");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_singleValue_true() throws Exception {
+			logTestStart();
+
+			String actual = formatter.format(new boolean[][] { { true } });
+
+			assertThat(actual).isEqualTo("▀");
+		}
+
+		/**
+		 * DOCME add JavaDoc for method test_twoRows
+		 * 
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_twoRows() throws Exception {
+			logTestStart();
+
+			String actual = formatter
+					.format(new boolean[][] { { false, true, false, true }, { false, false, true, true } });
+
+			assertThat(actual).isEqualTo(" ▀▄█");
+		}
+	}
+
+	/**
+	 * DOCME add JavaDoc for BooleanMatrixFormatterTest
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	@Nested
 	class Format_trueFalse_Test extends TestBase {
 
 		/**
