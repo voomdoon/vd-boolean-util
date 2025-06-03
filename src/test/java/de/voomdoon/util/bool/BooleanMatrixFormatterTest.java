d7007c36775c0f72521ae6b3257bdff4405dc35f
voomdoon <nilreb24-github20141105@yahoo.de>
2025-06-03 19:23:13 +0200

    test: refactor: rename classes

--- src/test/java/de/voomdoon/util/bool/BooleanMatrixFormatterTest.java
+++ src/test/java/de/voomdoon/util/bool/BooleanMatrixFormatterTest.java
@@ -223,298 +223,309 @@ class BooleanMatrixFormatterTest {
 	 *
 	 * @author André Schulz
 	 *
-	 * @since 0.1.0
+	 * @since DOCME add inception version number
 	 */
 	@Nested
-	class Format_1_0_Test extends TestBase {
+	class FormatTests {
 
 		/**
+		 * DOCME add JavaDoc for BooleanMatrixFormatterTest
+		 *
+		 * @author André Schulz
+		 *
 		 * @since 0.1.0
 		 */
-		@BeforeEach
-		void beforeEach_configureFormatter() {
-			builder.withFormat(Format.ONE_AND_ZERO);
+		@Nested
+		class DoubleWidthBlocksFormatTest extends TestBase {
+
+			/**
+			 * @since 0.1.0
+			 */
+			@BeforeEach
+			void beforeEach_configureFormatter() {
+				builder.withFormat(Format.DOUBLE_WIDTH_BLOCKS);
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_rowSeparator() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true }, { true } });
+
+				assertThat(actual).isEqualTo("██\n██");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_singleValue_false() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { false } });
+
+				assertThat(actual).isEqualTo("  ");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_singleValue_true() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true } });
+
+				assertThat(actual).isEqualTo("██");
+			}
 		}
 
 		/**
+		 * DOCME add JavaDoc for BooleanMatrixFormatterTest
+		 *
+		 * @author André Schulz
+		 *
 		 * @since 0.1.0
 		 */
-		@Test
-		void test_columnSeparator_none() throws Exception {
-			logTestStart();
+		@Nested
+		class HalfBlocksFormatTest extends TestBase {
 
-			String actual = format(new boolean[][] { { true, true } });
+			/**
+			 * @since 0.1.0
+			 */
+			@BeforeEach
+			void beforeEach_configureFormatter() {
+				builder.withFormat(Format.HALF_BLOCKS);
+			}
 
-			assertThat(actual).isEqualTo("11");
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_2Rows_falseFalse() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { false }, { false } });
+
+				assertThat(actual).isEqualTo(" ");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_2Rows_falseTrue() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { false }, { true } });
+
+				assertThat(actual).isEqualTo("▄");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_2Rows_trueFalse() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true }, { false } });
+
+				assertThat(actual).isEqualTo("▀");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_2Rows_trueTrue() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true }, { true } });
+
+				assertThat(actual).isEqualTo("█");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_3Rows() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true }, { true }, { true } });
+
+				assertThat(actual).isEqualTo("█\n▀");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_singleValue_false() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { false } });
+
+				assertThat(actual).isEqualTo(" ");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_singleValue_true() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true } });
+
+				assertThat(actual).isEqualTo("▀");
+			}
+
+			/**
+			 * DOCME add JavaDoc for method test_twoRows
+			 * 
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_twoRows() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { false, true, false, true }, { false, false, true, true } });
+
+				assertThat(actual).isEqualTo(" ▀▄█");
+			}
 		}
 
 		/**
+		 * DOCME add JavaDoc for BooleanMatrixFormatterTest
+		 *
+		 * @author André Schulz
+		 *
 		 * @since 0.1.0
 		 */
-		@Test
-		void test_rowSeparator() throws Exception {
-			logTestStart();
+		@Nested
+		class OneZeroFormatTest extends TestBase {
 
-			String actual = format(new boolean[][] { { true }, { true } });
+			/**
+			 * @since 0.1.0
+			 */
+			@BeforeEach
+			void beforeEach_configureFormatter() {
+				builder.withFormat(Format.ONE_AND_ZERO);
+			}
 
-			assertThat(actual).isEqualTo("1\n1");
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_columnSeparator_none() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true, true } });
+
+				assertThat(actual).isEqualTo("11");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_rowSeparator() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true }, { true } });
+
+				assertThat(actual).isEqualTo("1\n1");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_singleValue_false() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { false } });
+
+				assertThat(actual).isEqualTo("0");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_singleValue_true() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true } });
+
+				assertThat(actual).isEqualTo("1");
+			}
 		}
 
 		/**
+		 * DOCME add JavaDoc for BooleanMatrixFormatterTest
+		 *
+		 * @author André Schulz
+		 *
 		 * @since 0.1.0
 		 */
-		@Test
-		void test_singleValue_false() throws Exception {
-			logTestStart();
+		@Nested
+		class TrueFalseFormatTest extends TestBase {
 
-			String actual = format(new boolean[][] { { false } });
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_columnSeparator_default() throws Exception {
+				logTestStart();
 
-			assertThat(actual).isEqualTo("0");
+				String actual = format(new boolean[][] { { true, true } });
+
+				assertThat(actual).isEqualTo("true,true");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_rowSeparator() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true }, { true } });
+
+				assertThat(actual).isEqualTo("true\ntrue");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_singleValue_false() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { false } });
+
+				assertThat(actual).isEqualTo("false");
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			@Test
+			void test_singleValue_true() throws Exception {
+				logTestStart();
+
+				String actual = format(new boolean[][] { { true } });
+
+				assertThat(actual).isEqualTo("true");
+			}
+
 		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_singleValue_true() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true } });
-
-			assertThat(actual).isEqualTo("1");
-		}
-	}
-
-	/**
-	 * DOCME add JavaDoc for BooleanMatrixFormatterTest
-	 *
-	 * @author André Schulz
-	 *
-	 * @since 0.1.0
-	 */
-	@Nested
-	class Format_doubleWidthBlocks_Test extends TestBase {
-
-		/**
-		 * @since 0.1.0
-		 */
-		@BeforeEach
-		void beforeEach_configureFormatter() {
-			builder.withFormat(Format.DOUBLE_WIDTH_BLOCKS);
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_rowSeparator() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true }, { true } });
-
-			assertThat(actual).isEqualTo("██\n██");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_singleValue_false() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { false } });
-
-			assertThat(actual).isEqualTo("  ");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_singleValue_true() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true } });
-
-			assertThat(actual).isEqualTo("██");
-		}
-	}
-
-	/**
-	 * DOCME add JavaDoc for BooleanMatrixFormatterTest
-	 *
-	 * @author André Schulz
-	 *
-	 * @since 0.1.0
-	 */
-	@Nested
-	class Format_halfBlocks_Test extends TestBase {
-
-		/**
-		 * @since 0.1.0
-		 */
-		@BeforeEach
-		void beforeEach_configureFormatter() {
-			builder.withFormat(Format.HALF_BLOCKS);
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_2Rows_falseFalse() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { false }, { false } });
-
-			assertThat(actual).isEqualTo(" ");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_2Rows_falseTrue() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { false }, { true } });
-
-			assertThat(actual).isEqualTo("▄");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_2Rows_trueFalse() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true }, { false } });
-
-			assertThat(actual).isEqualTo("▀");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_2Rows_trueTrue() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true }, { true } });
-
-			assertThat(actual).isEqualTo("█");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_3Rows() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true }, { true }, { true } });
-
-			assertThat(actual).isEqualTo("█\n▀");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_singleValue_false() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { false } });
-
-			assertThat(actual).isEqualTo(" ");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_singleValue_true() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true } });
-
-			assertThat(actual).isEqualTo("▀");
-		}
-
-		/**
-		 * DOCME add JavaDoc for method test_twoRows
-		 * 
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_twoRows() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { false, true, false, true }, { false, false, true, true } });
-
-			assertThat(actual).isEqualTo(" ▀▄█");
-		}
-	}
-
-	/**
-	 * DOCME add JavaDoc for BooleanMatrixFormatterTest
-	 *
-	 * @author André Schulz
-	 *
-	 * @since 0.1.0
-	 */
-	@Nested
-	class Format_trueFalse_Test extends TestBase {
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_columnSeparator_default() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true, true } });
-
-			assertThat(actual).isEqualTo("true,true");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_rowSeparator() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true }, { true } });
-
-			assertThat(actual).isEqualTo("true\ntrue");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_singleValue_false() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { false } });
-
-			assertThat(actual).isEqualTo("false");
-		}
-
-		/**
-		 * @since 0.1.0
-		 */
-		@Test
-		void test_singleValue_true() throws Exception {
-			logTestStart();
-
-			String actual = format(new boolean[][] { { true } });
-
-			assertThat(actual).isEqualTo("true");
-		}
-
 	}
 
 	/**
