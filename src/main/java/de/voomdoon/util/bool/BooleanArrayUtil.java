0c51733099a21c5729debe21e9a226395b57b4db
voomdoon <nilreb24-github20141105@yahoo.de>
2025-06-05 22:05:04 +0200

    feat: add BooleanArrayUtil.countTrue
    references: #6

---
+++ src/main/java/de/voomdoon/util/bool/BooleanArrayUtil.java
@@ -0,0 +1,37 @@
+package de.voomdoon.util.bool;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+public class BooleanArrayUtil {
+
+	/**
+	 * DOCME add JavaDoc for method countTrue
+	 * 
+	 * @param array
+	 * @return
+	 * @since 0.1.0
+	 */
+	public static int countTrue(boolean[] array) {
+		int count = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i]) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	private BooleanArrayUtil() {
+		// nothing to do
+	}
+}
---
+++ src/test/java/de/voomdoon/util/bool/BooleanArrayUtilTest.java
@@ -0,0 +1,85 @@
+package de.voomdoon.util.bool;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import de.voomdoon.testing.tests.TestBase;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+class BooleanArrayUtilTest {
+
+	/**
+	 * Tests for {@link BooleanArrayUtil#countTrue(boolean[])}.
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	@Nested
+	class CountTrueTest extends TestBase {
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_empty_result0() throws Exception {
+			logTestStart();
+
+			boolean[] array = new boolean[] {};
+
+			int actual = BooleanArrayUtil.countTrue(array);
+
+			assertThat(actual).isZero();
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_false_result0() throws Exception {
+			logTestStart();
+
+			boolean[] array = new boolean[] { false };
+
+			int actual = BooleanArrayUtil.countTrue(array);
+
+			assertThat(actual).isZero();
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_true1_result1() throws Exception {
+			logTestStart();
+
+			boolean[] array = new boolean[] { true };
+
+			int actual = BooleanArrayUtil.countTrue(array);
+
+			assertThat(actual).isEqualTo(1);
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_true2_result2() throws Exception {
+			logTestStart();
+
+			boolean[] array = { true, true };
+
+			int actual = BooleanArrayUtil.countTrue(array);
+
+			assertThat(actual).isEqualTo(2);
+		}
+	}
+}
